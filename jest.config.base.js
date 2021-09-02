8a7fafc38c22f3c1855532a91df6984cbb73cc0e
Hammad Jutt <jutt@ualberta.ca>
2021-09-01 21:51:59 -0600

    Enforce consistent timezone in jest tests

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   setupFiles: ['jest-date-mock'],
+  globalSetup: '../../../global.setup.js',
   testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
   collectCoverage: true,
   coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$'],
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const base = require('./jest.config.base.js');
 
 module.exports = {
   ...base,
+  globalSetup: './global.setup.js',
   projects: ['<rootDir>/{packages,apps}/*/jest.config.cjs'],
   coverageDirectory: '<rootDir>/coverage/',
 };
