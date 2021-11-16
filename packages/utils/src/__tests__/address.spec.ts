ae6aa13d7904967e0e8d6ce5e8c32659e32fdbca
Hammad Jutt <jutt@ualberta.ca>
2021-11-16 15:19:46 -0700

    Fix tests for address utils

--- packages/utils/src/__tests__/address.spec.ts
+++ packages/utils/src/__tests__/address.spec.ts
@@ -1,36 +1,45 @@
-import { shortenIfAddress } from '../address';
+import { shortenAddress, shortenIfAddress } from '../address';
 
-describe('shortenIfAddress', () => {
+describe('address', () => {
   const testAddress = '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B';
 
-  it('should return an empty string if given address is falsy', () => {
-    expect(shortenIfAddress('')).toBe('');
-    expect(shortenIfAddress(null)).toBe('');
-    expect(shortenIfAddress(undefined)).toBe('');
+  describe('shortenIfAddress', () => {
+    it('should return an empty string if given address is falsy', () => {
+      expect(shortenIfAddress('')).toBe('');
+      expect(shortenIfAddress(null)).toBe('');
+      expect(shortenIfAddress(undefined)).toBe('');
+    });
+
+    it('should shorten the given Ethereum address', () => {
+      expect(shortenIfAddress(testAddress)).toBe('0xAb58...eC9B');
+      expect(shortenIfAddress(testAddress.toLowerCase())).toBe('0xAb58...eC9B');
+    });
+
+    it('should return the string unchanged if not given a valid address', () => {
+      expect(shortenIfAddress('foo.eth')).toBe('foo.eth');
+      expect(shortenIfAddress(`${testAddress}1`)).toBe(`${testAddress}1`);
+    });
   });
 
-  it('should shorten the given Ethereum address', () => {
-    expect(shortenIfAddress(testAddress)).toBe('0xAb58...eC9B');
-    expect(shortenIfAddress(testAddress.toLowerCase())).toBe('0xAb58...eC9B');
-  });
+  describe('shortenAddress', () => {
+    it('should throw error if given address is not valid', () => {
+      const invalidAddress = '0xFb5801a7D398351b8bE11C439e05C5B3259aeC9B';
 
-  it('should throw error if given address is not valid', () => {
-    const invalidAddress = '0xFb5801a7D398351b8bE11C439e05C5B3259aeC9B';
-
-    expect(() => {
-      shortenIfAddress(`${testAddress}1`);
-    }).toThrowErrorMatchingInlineSnapshot(
-      `"Invalid input, address can't be parsed"`,
-    );
-    expect(() => {
-      shortenIfAddress(invalidAddress);
-    }).toThrowErrorMatchingInlineSnapshot(
-      `"Invalid input, address can't be parsed"`,
-    );
-    expect(() => {
-      shortenIfAddress('foo');
-    }).toThrowErrorMatchingInlineSnapshot(
-      `"Invalid input, address can't be parsed"`,
-    );
+      expect(() => {
+        shortenAddress(`${testAddress}1`);
+      }).toThrowErrorMatchingInlineSnapshot(
+        `"Invalid input, address can't be parsed"`,
+      );
+      expect(() => {
+        shortenAddress(invalidAddress);
+      }).toThrowErrorMatchingInlineSnapshot(
+        `"Invalid input, address can't be parsed"`,
+      );
+      expect(() => {
+        shortenAddress('foo');
+      }).toThrowErrorMatchingInlineSnapshot(
+        `"Invalid input, address can't be parsed"`,
+      );
+    });
   });
 });
